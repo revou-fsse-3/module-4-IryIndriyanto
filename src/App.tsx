a153632ac69ddd2b23d0662358ed779cee2aa90d
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-01-01 00:00:57 -0700

    build all form in one place

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
-  
-  return (
-    <h1 className=" text-3xl text-red-500 font-bold">Hello World!!</h1>
+import MultiStepForm from "./form/MultiStepForm";
 
-  )
+function App() {
+  return (
+    <div className=" flex justify-center items-center ">
+      <MultiStepForm />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/form/AccountForm.tsx
@@ -0,0 +1,168 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const AccountForm = () => {
+  const formSchema = yup.object().shape({
+    fullName: yup.string().min(2, "Full Name must be at least 2 characters."),
+    email: yup.string().email().required(),
+    birthDate: yup.date().required("it is a required field"),
+    streetAddress: yup.string().required(),
+  });
+
+  type formValues = yup.InferType<typeof formSchema>;
+  const form = useForm<formValues>({
+    resolver: yupResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      streetAddress: "",
+    },
+  });
+
+  const onSubmit = (data: formValues) => {
+    alert(JSON.stringify(data));
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card className=" w-[350px] p-4 space-y-6">
+          {/* Personal Information */}
+          <CardHeader>
+            <CardTitle className="text-center">Personal Information</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Full Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Input Your Full Name" {...field} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Input Your Email" {...field} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="birthDate"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Date of birth</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardHeader>
+            <CardTitle className="text-center">Address Information</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="streetAddress"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Street Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Input Your Street Address" {...field} />
+                  </FormControl>
+                  <FormDescription />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            {/* <Button variant={"outline"} type="submit">
+              Submit
+            </Button> */}
+            <Button type="submit">Submit</Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  );
+};
+
+export default AccountForm;
