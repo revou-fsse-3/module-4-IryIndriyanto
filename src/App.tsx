76ec57072fa82364a044288b4337035547ffc386
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-01-02 04:04:42 -0700

    change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import 'react-day-picker/dist/style.css';
 
 function App() {
   return (
-    <div className=" flex justify-center items-center h-screen">
+    <div className=" flex justify-center items-center h-screen bg-slate-200">
       <MultiStepForm />
     </div>
   );
